c97061a0bb8f5a972c15e99f657d16f50bedb0c5
banguncreative <banguncreative.id@gmail.com>
2023-08-15 14:58:36 +0700

    organize GAS file, adding Controller and initial script

--- readme.md
+++ readme.md
@@ -1,5 +1,9 @@
+![GB_sources](https://4.bp.blogspot.com/-AbJ1au7SfYc/XEHifQKXZWI/AAAAAAAAA5I/beXM7mmhipUNkWUq0zwEmJjOtdg-XZoRgCK4BGAYYCw/s320/chanel%2Bart.png)
+
 GB-uploadImg
 ============
 
 > an opensources form addon direct upload image to Discord CDN and return url back to the form.
 
+# Initial
+- soon
---
+++ src/gb-upladImg.js
@@ -0,0 +1,65 @@
+/**
+ * GB-uploadImg
+ * @description GB-uploadImg controller and init script
+ * @param   {String}    token   Google Appscript webapp token
+ * @param   {String}    channel_id  Discord channel id  
+ */
+
+class gb_upload {
+    constructor(token, channel_id) {
+        this.base = this.c_uri(token);
+        this.channel_id = channel_id;
+    }
+
+    init() {
+        const base = this.base,
+            channel_id = this.channel_id,
+            makeid = this.makeid;
+        document
+        .querySelectorAll(".gb-uploadimg")
+        .forEach(function(e){
+            const server = base + "?m=uploadimg&channel_id="+channel_id+"&q="+ makeid(8)
+            var [input, button] = e.children;
+            button.onclick = function(){
+            var win = window.open(server, "_blank");
+            var timer = setInterval(async function() {
+                button.innerText = "please await..."
+                button.disabled = true;
+                if (win.closed) {
+                clearInterval(timer);
+                var exchange = server.replace("uploadimg", "exchangeimg");
+                var res = await fetch(exchange);
+                res = await res.json();
+
+                if(res.data !== []){
+                    input.value = res.data[1];
+                    button.innerText = "done"
+                } else {
+                    button.innerText = "upload";
+                    button.disabled = "false";
+                }
+                // console.log(button)
+                // alert('closed');
+                }
+            }, 1000);
+            }
+        })
+    }
+
+    c_uri(_t) {
+        var b = "\x68\x74\x74\x70\x73\x3a\x2f\x2f\x73\x63\x72\x69\x70\x74\x2e\x67\x6f\x6f\x67\x6c\x65\x2e\x63\x6f\x6d\x2f\x6d\x61\x63\x72\x6f\x73\x2f\x73\x2f";
+        return b + _t + atob('L2V4ZWM=');
+    }
+
+    makeid(length) {
+        let result = '';
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        let counter = 0;
+        while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+        }
+        return result;
+    }
+}
